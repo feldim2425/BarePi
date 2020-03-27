00324f19a5980945768a7686f000b43762f1df9d
feldim2425 <feldim2425@gmail.com>
2020-03-27 03:54:21 +0100

    Some work on the bootup sequence ... still WIP Finally got it to show up in the binary at the right location

--- src/start.c
+++ src/start.c
@@ -16,24 +16,24 @@ extern initf_t __fini_array_end;
 
 void exit(int exitcode)
 {
-    asm volatile("dsb");
+    //asm volatile("dsb");
     semihost_exit(ADP_Stopped_ApplicationExit, exitcode);
     while (1)
         ;
 }
 
-void _initialize()
+void _sysinit()
 {
     initf_t *it;
 
     for (it = &__preinit_array_start; it < &__preinit_array_end; ++it)
     {
-        (*it)();
+        (**it)();
     }
 
     for (it = &__init_array_start; it < &__init_array_end; ++it)
     {
-        (*it)();
+        (**it)();
     }
 
     exit(main());
