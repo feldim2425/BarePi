43008a902388dc07f94615bc0681366ad9c731af
feldim2425 <feldim2425@gmail.com>
2020-04-07 04:12:12 +0200

    Console test

--- test/main.c
+++ test/main.c
@@ -1,5 +1,10 @@
+#include <barepi/console/console.h>
 
-
-int main(void){
+int main(void)
+{
+    console_t con;
+    console_open(CONSOLET_SEMIHOST, &con);
+    console_puts("Hello World from main()\n", &con, false);
+    console_close(&con);
     return 0;
 }
